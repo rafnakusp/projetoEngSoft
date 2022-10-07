e29034788a920b235098171b50f2753faa344cf5
Gabriel Coutinho <gabrielcoutribeiro@usp.br>
2022-10-07 17:39:32 -0300

    models

--- source/MyProj/book/models.py
+++ source/MyProj/book/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+
+class Rota(models.Model):
+ rota_id = models.IntegerField(primary_key=True)
+ outro_aeroporto = models.CharField(max_length=50, null=False)
+ chegada = models.BooleanField(null=False)
+ class Meta:
+   db_table = 'rota'
+
+class Voo(models.Model):
+    voo_id = models.IntegerField(primary_key=True)
+    companhia_aerea = models.CharField(max_length=255, null=False)
+    horario_partida_previsto = models.DateTimeField(null=False)
+    horario_chegada_previsto = models.DateTimeField(null=False)
+    rota_voo = models.ForeignKey(Rota, on_delete=models.CASCADE)
+    class Meta:
+      db_table = 'voo'
+
+class Status(models.Model):
+    status_id = models.IntegerField(primary_key=True)
+    status_nome = models.CharField(max_length=255, null=False)
+    class Meta:
+        db_table = 'status'
+
+class ProgressoVoo(models.Model):
+ progresso_id = models.IntegerField(primary_key=True)
+ status_voo = models.ForeignKey(Status, on_delete=models.CASCADE)
+ voo = models.ForeignKey(Voo, on_delete=models.CASCADE, null=False)
+ horario_partida_real = models.DateTimeField(null=True)
+ horario_chegada_real = models.DateTimeField(null=True)
+ class Meta:
+   db_table = 'progressoVoo' 
