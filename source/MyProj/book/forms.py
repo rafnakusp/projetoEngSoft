769a3034c6cc644998303998a969cb50a86af54c
Rafael Nakata <rafael.nakata@usp.br>
2022-10-29 19:21:52 -0300

    Habilita campos em branco no filtro do CRUD

--- source/MyProj/book/forms.py
+++ source/MyProj/book/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Voo
 
 class formularioCadastroVoo(forms.Form):
-    companhia = forms.CharField(max_length=50)
-    horario_partida = forms.DateTimeField()
-    horario_chegada = forms.DateTimeField()
-    rota = forms.CharField(max_length=50)
+    companhia = forms.CharField(max_length=50, required=False)
+    horario_partida = forms.DateTimeField(required=False)
+    horario_chegada = forms.DateTimeField(required=False)
+    rota = forms.CharField(max_length=50, required=False)
