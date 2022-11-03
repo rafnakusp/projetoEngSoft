ef96b73dd3b06b0634c34d0ac7822db4d6012647
GabrielCout <gabrielcoutribeiro@usp.br>
2022-11-02 21:34:46 -0300

    Relatorio de voos realizados mostra somente os voos realizados (com horario de chegada real)

--- source/MyProj/book/views.py
+++ source/MyProj/book/views.py
@@ -457,7 +457,7 @@ def geracaoDeRelatoriosVoosRealizados(request):
         timestamp_max = form.data['timestamp_max']
 
         context = {
-            "progressovoo_list": controleGeracaoRelatorios.filtrarVoos(timestamp_min, timestamp_max)
+            "progressovoo_list": controleGeracaoRelatorios.filtrarVoosRealizados(timestamp_min, timestamp_max)
         }
         return render(request, "relatoriovoosrealizados.html", context)
 
@@ -497,6 +497,9 @@ class ControleGeracaoRelatorios():
         return self.filtrarVoos(timestamp_min, timestamp_max).filter(Q(horario_chegada_real__gt=F('voo__horario_chegada_previsto')) | Q(horario_chegada_real__isnull=True, \
             horario_partida_real__gt=F('voo__horario_partida_previsto')) | Q(horario_partida_real__isnull=True, voo__horario_partida_previsto__lt=agora) | \
             Q(horario_chegada_real__isnull=True, voo__horario_chegada_previsto__lt=agora)).order_by('voo_id').distinct()
+
+    def filtrarVoosRealizados(self, timestamp_min, timestamp_max):
+        return self.filtrarVoos(timestamp_min, timestamp_max).filter(horario_chegada_real__isnull=False).order_by('voo_id').distinct()
 
 ################################################################################
 ####                         Criador de tabelas                             ####
